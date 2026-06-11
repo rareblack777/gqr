2d2a9bbdbd8077acd638a12adfb902071c70c45f
Leandro Silva Ramalho <contato.lsr82@gmail.com>
2026-06-11 10:56:23 -0300

    Atualizando URL de destino no script do QR Code

--- gqr.py
+++ gqr.py
@@ -1,11 +1,13 @@
 import qrcode
 
-# O segredo: o link aqui é o da sua página própria (ex: seu-dominio.com/hub.html)
-link_do_seu_hub = "https://seusite.com/hub.html" 
+# Alteração necessária: link apontando para o projeto correto
+link = "https://rareblack777.github.io/resineimcz/social.html"
 
 qr = qrcode.QRCode(version=1, box_size=15, border=2)
-qr.add_data(link_do_seu_hub)
+qr.add_data(link)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="#d4af37", back_color="#0a0f14")
-img.save("qrcode_hub_resinei.png")
+img.save("qrcode_siga_resinei.png")
+
+print("QR Code gerado com sucesso!")
